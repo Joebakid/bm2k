27b13c263eff3342017877b8e8db6b720600bd30
joebakid <josephbawo@gmail.com>
2023-12-08 23:38:43 +0100

    all

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 const menu = document.querySelector(".menu");
 
-menu.addEventListener("click", function () {});
+menu.addEventListener("click", function () {
+  document.querySelector(".hamburger-menu").style.display = "flex";
+});
+
+const closeBtn = document.querySelector(".close-btn");
+
+closeBtn.addEventListener("click", function () {
+  document.querySelector(".hamburger-menu").style.display = "none";
+});
