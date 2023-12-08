175f0234332d1cce7ae563adccf8a0130367adff
joebakid <josephbawo@gmail.com>
2023-12-08 23:43:42 +0100

    testing phase

--- script.js
+++ script.js
@@ -2,10 +2,14 @@ const menu = document.querySelector(".menu");
 
 menu.addEventListener("click", function () {
   document.querySelector(".hamburger-menu").style.display = "flex";
+  document.querySelector(".hamburger-menu").style.transition =
+    "all 1s ease-in-out";
 });
 
 const closeBtn = document.querySelector(".close-btn");
 
 closeBtn.addEventListener("click", function () {
   document.querySelector(".hamburger-menu").style.display = "none";
+  document.querySelector(".hamburger-menu").style.transition =
+    "all 1s ease-in-out";
 });
