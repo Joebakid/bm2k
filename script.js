89a36cc4302664fd641a781b375e5c2ab3e63abb
joebakid <josephbawo@gmail.com>
2023-12-15 17:51:37 +0100

    accordion

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
- 
+const mobileMenu = document.querySelector(".mobile-menu");
+const hamburgerMenu = document.querySelector(".hamburger");
+
+const accordion = document.querySelector(".accordion");
+
+accordion.addEventListener("click", (e) => {
+  const activePanel = e.target.closest(".accordion-panel");
+  if (!activePanel) return;
+  toggleAccordion(activePanel);
+});
+
+function toggleAccordion(panelToActivate) {
+  const activeButton = panelToActivate.querySelector("button");
+  const activePanel = panelToActivate.querySelector(".accordion-content");
+  const activePanelIsOpened = activeButton.getAttribute("aria-expanded");
+
+  if (activePanelIsOpened === "true") {
+    panelToActivate
+      .querySelector("button")
+      .setAttribute("aria-expanded", false);
+
+    panelToActivate
+      .querySelector(".accordion-content")
+      .setAttribute("aria-hidden", true);
+  } else {
+    panelToActivate.querySelector("button").setAttribute("aria-expanded", true);
+
+    panelToActivate
+      .querySelector(".accordion-content")
+      .setAttribute("aria-hidden", false);
+  }
+}
