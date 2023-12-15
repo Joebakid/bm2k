54d096de19bb6aa4972599f2fdee17ed74a9a497
joebakid <josephbawo@gmail.com>
2023-12-15 20:16:22 +0100

    navbar

--- script.js
+++ script.js
@@ -30,3 +30,14 @@ function toggleAccordion(panelToActivate) {
       .setAttribute("aria-hidden", false);
   }
 }
+
+// NAV
+
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleButton = document.getElementById("navbar-toggle");
+  const navbarLinks = document.getElementById("navbar-links");
+
+  toggleButton.addEventListener("click", function () {
+    navbarLinks.classList.toggle("show");
+  });
+});
